657d6f690921cc320a8a2909ff5567211f4c92d4
Sean-V <sean.vanderbach@colorado.edu>
2019-10-02 13:21:57 -0600

    maps networkx addition

--- maps.py
+++ maps.py
@@ -1,7 +1,13 @@
 #this file contain the map dictionary. The structure of this dictionary is as follows:
-maps = {
-    'COASTLINE' : {
-        'EXTMAINENTRANCE' : ['EXTPOOL'],
-        'EXTPOOL' : ['EXTMAINENTRANCE']
-    }
-}
+import networkx as nx
+import matplotlib.pyplot as plt
+coastline = nx.DiGraph()
+coastline.add_node('COASTLINE')
+coastline.add_edges_from([
+    ('COASTLINE', 'EXTMAINENTRANCE'), ('COASTLINE', 'EXTPOOLSIDE'), ('COASTLINE', 'EXTRUINS'),
+    ('EXTMAINENTRANCE', 'EXTPOOL'), ('EXTMAINENTRANCE', 'EXTBACKALLEY'), ('EXTMAINENTRANCE', 'EXTGARAGEROOF'), ('EXTMAINENTRANCE', 'EXTSOUTHPROMENADE'), ('EXTMAINENTRANCE', 'EXTROOFTOP'), ('EXTMAINENTRANCE', '2FHALLWAY'), ('EXTMAINENTRANCE', '1FSERVICEENTRANCE'), ('EXTMAINENTRANCE', '2FPENTHOUSE'), ('EXTMAINENTRANCE', '1FMAINLOBBY'),
+    ('EXTGARAGEROOF', 'EXTBACKALLEY'), ('EXTGARAGEROOF', 'EXTPOOL'), ('EXTGARAGEROOF', 'EXTMAINENTRANCE'),
+    ('EXTBACKALLEY', 'EXTGARAGEROOF'), ('EXTBACKALLEY', 'EXTPOOL'), ('EXTBACKALLEY', 'EXTMAINENTRANCE')
+])
+nx.draw(coastline, with_labels=True)
+plt.show()
